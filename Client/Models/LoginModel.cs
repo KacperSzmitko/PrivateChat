50deb0cdf9b405a98d2f1c35f2ac2301333b2ced
Marcin Tajsner <marcin.tajsner@gmail.com>
2021-03-14 22:02:54 +0100

    User search for friend invitation - done

--- Client/Models/LoginModel.cs
+++ Client/Models/LoginModel.cs
@@ -7,13 +7,19 @@ namespace Client.Models
 {
     public class LoginModel : BaseModel
     {
+        private string username;
+        private string pass;
+
+        public string Username { get { return username; } set { username = value; } }
+        public string Pass { get { return pass; } set { pass = value; } }
+
+        public LoginModel(ServerConnection connection) : base(connection) {}
+        
         public string LoginUser(string username, string password) {
             int response = ServerCommands.LoginCommand(ref connection, username, password);
             if (response == (int)ErrorCodes.NO_ERROR) return username;
             else if (response == (int)ErrorCodes.USER_NOT_FOUND || response == (int)ErrorCodes.INCORRECT_PASSWORD) return null;
             else throw new Exception(GetErrorCodeName(response)); 
         }
-
-        public LoginModel(ServerConnection connection) : base(connection) {}
     }
 }
--- Client/ViewModels/ChatViewModel.cs
+++ Client/ViewModels/ChatViewModel.cs
@@ -1,8 +1,11 @@
-﻿using Client.Models;
+﻿using Client.Commands;
+using Client.Models;
 using Newtonsoft.Json;
 using Shared;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows.Input;
 
 namespace Client.ViewModels
 {
@@ -10,19 +13,100 @@ namespace Client.ViewModels
     {
         private ChatModel model;
 
-        private readonly string username;
+        private Thread sendInvitationThread;
 
-        private ObservableCollection<Friend> friendsList;
+        private RelayCommand sendInvitationCommand;
 
-        public string Username { get { return username; } }
+        private InvitationStatus lastInvitationStatus;
 
-        public ObservableCollection<Friend> FriendsList { get { return friendsList; } }
+        public string Username { get { return model.Username; } }
+
+        public string InvitationUsername {
+            get { return model.InvitationUsername; }
+            set {
+                if (value != model.InvitationUsername) {
+                    model.InvitationUsername = value;
+                    OnPropertyChanged(nameof(InvitationUsername));
+                }
+            }
+        }
+
+        public List<Friend> FriendsList { get { return model.FriendsList; } }
+
+        public string UserNotFoundErrorVisibility {
+            get {
+                if (lastInvitationStatus == InvitationStatus.USER_NOT_FOUND) return "Visible";
+                else return "Collapsed";
+            }
+        }
+        public string UserAlreadyAFriendErrorVisibility {
+            get {
+                if (lastInvitationStatus == InvitationStatus.USER_ALREADY_A_FRIEND) return "Visible";
+                else return "Collapsed";
+            }
+        }
+        public string InvitationSentInfoVisibility {
+            get {
+                if (lastInvitationStatus == InvitationStatus.INVITATION_SENT) return "Visible";
+                else return "Collapsed";
+            }
+        }
+
+
+        public ICommand SendInvitationCommand {
+            get {
+                if (sendInvitationCommand == null) {
+                    sendInvitationCommand = new RelayCommand(_ => {
+                        sendInvitationThread = new Thread(SendInvitationAsync);
+                        sendInvitationThread.Start();
+                    }, _ => {
+                        if (lastInvitationStatus == InvitationStatus.NO_INVITATION && model.CheckUsernameText(model.InvitationUsername)) return true;
+                        else return false;
+                    });
+                }
+                return sendInvitationCommand;
+            }
+        }
 
         public ChatViewModel(ServerConnection connection, Navigator navigator, string username) : base(connection, navigator) {
             this.model = new ChatModel(this.connection);
-            this.username = username;
-            friendsList = new ObservableCollection<Friend>(JsonConvert.DeserializeObject<List<Friend>>(this.model.GetFriendsJSON()));
+            this.model.Username = username;
+            this.model.FriendsList = JsonConvert.DeserializeObject<List<Friend>>(this.model.GetFriendsJSON());
+            this.lastInvitationStatus = InvitationStatus.NO_INVITATION;
+            OnPropertyChanged(nameof(FriendsList));
+        }
 
+        private void SendInvitationAsync() {
+            lastInvitationStatus = InvitationStatus.WAITING_FOR_RESPONSE;
+
+            bool userExists = model.CheckUserExist(model.InvitationUsername);
+
+            if (!userExists) lastInvitationStatus = InvitationStatus.USER_NOT_FOUND;
+            else {
+                bool userAlredyAFriend = false;
+                foreach (Friend f in model.FriendsList) {
+                    if (f.Username == model.InvitationUsername) userAlredyAFriend = true;
+                }
+                if (userAlredyAFriend) lastInvitationStatus = InvitationStatus.USER_ALREADY_A_FRIEND;
+                else {
+                    //Send invitation
+                    lastInvitationStatus = InvitationStatus.INVITATION_SENT;
+                }
+            }
+
+            OnPropertyChanged(nameof(UserNotFoundErrorVisibility));
+            OnPropertyChanged(nameof(UserAlreadyAFriendErrorVisibility));
+            OnPropertyChanged(nameof(InvitationSentInfoVisibility));
+
+            Thread.Sleep(5000);
+
+            model.InvitationUsername = "";
+            lastInvitationStatus = InvitationStatus.NO_INVITATION;
+
+            OnPropertyChanged(nameof(UserNotFoundErrorVisibility));
+            OnPropertyChanged(nameof(UserAlreadyAFriendErrorVisibility));
+            OnPropertyChanged(nameof(InvitationSentInfoVisibility));
+            OnPropertyChanged(nameof(InvitationUsername));
         }
     }
 }
