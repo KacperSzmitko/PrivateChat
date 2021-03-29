e91de392e687ddc17d23eff915cea9d4f634553d
Marcin Tajsner <marcin.tajsner@gmail.com>
2021-03-29 19:58:39 +0200

    log

---
+++ Client/Log.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Client
+{
+    public static class Log
+    {
+        private const string appLocalDataFolderName = "private_chat";
+        private static readonly string logFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), appLocalDataFolderName);
+        private static string logFileName;
+
+        public static void SetLogFileName(string fileName) {
+            logFileName = fileName;
+        }
+
+        public static void LogText(string text) {
+            string fullPath = Path.Combine(logFilePath, logFileName);
+            File.AppendAllText(fullPath, text + Environment.NewLine);
+        }
+
+    }
+}
