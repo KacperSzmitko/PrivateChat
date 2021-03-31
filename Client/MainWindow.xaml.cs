e8a888df2a5b02cdeff46ff913949f16c0d4c752
Marcin Tajsner <marcin.tajsner@gmail.com>
2021-03-31 19:41:42 +0200

    client ip change

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using Client.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Client
 {
@@ -20,7 +8,7 @@ namespace Client
     {
         public MainWindow() {
             InitializeComponent();
-            ServerConnection connection = new ServerConnection("localhost", 13579);
+            ServerConnection connection = new ServerConnection("bd-projekt.ddns.net", 13579);
             Navigator navigator = new Navigator();
             MainViewModel viewModel = new MainViewModel(connection, navigator);
             navigator.ViewChanged += viewModel.OnViewChanged;
